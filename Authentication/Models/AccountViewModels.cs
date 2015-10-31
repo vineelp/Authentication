23f014e0e53dbc58d62331f49a6af97f75a7ee0a
vineelp <vineelp@gmail.com>
2015-10-31 10:28:35 -0400

    added client side validation

--- Authentication/Models/AccountViewModels.cs
+++ Authentication/Models/AccountViewModels.cs
@@ -15,6 +15,7 @@ namespace Authentication.Models
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [MinLength(3)]
         public string Password { get; set; }
     }
 }
