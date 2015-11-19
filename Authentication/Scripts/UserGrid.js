5aa6811e4d650d5fedf88dd2d14065d73a6f510c
vineelp <vineelp@gmail.com>
2015-11-19 17:39:14 -0500

    added dblclick for edit

--- Authentication/Scripts/UserGrid.js
+++ Authentication/Scripts/UserGrid.js
@@ -1,6 +1,32 @@
-﻿var dbEditWidth = 30;
-var dbWidth = 400;
-$(function () {
+﻿$(function () {
+    var dbEditWidth = 30;
+    var dbWidth = 400;
+    var editOptions = {
+        url: '/Account/EditUser',
+        width: dbWidth,
+        top: Math.max(0, ($(window).height() / 3)),
+        left: Math.max(0, ($(window).width() / 3)),
+        closeOnEscape: true,
+        beforeShowForm: function (formid) {
+            $('#editmodgrid  input').keyup(function (event) {
+                if (event.keyCode == 13) {
+                    $("#sData").click();
+                }
+            });
+        },
+        afterComplete: function (response) {
+            if (response.responseText) {
+                $(this).jqGrid('setGridParam', { datatype: 'json' }).trigger('reloadGrid');
+                if (response.responseText.indexOf("Saved") > -1) {
+                    $('#cData').trigger('click');
+                    alert(response.responseText);
+                } else {
+                    $("#FormError").show();
+                    $("#FormError>td").html(response.responseText);
+                }
+            }
+        }
+    };
     $("#grid").jqGrid({
         url: "/Account/GetUsersList",
         datatype: 'json',
@@ -14,39 +40,24 @@ $(function () {
                         keys: true,
                         delbutton: false,
                         editformbutton: true,
-                        editOptions: {
-                            url: '/Account/EditUser',
-                            width: dbWidth,
-                            top: Math.max(0, ($(window).height() / 3)),
-                            left: Math.max(0, ($(window).width() / 3)),
-                            closeOnEscape: true,
-                            afterComplete: function (response) {
-                                if (response.responseText) {
-                                    $(this).jqGrid('setGridParam', { datatype: 'json' }).trigger('reloadGrid');
-                                    if (response.responseText.indexOf("Success") > -1) {
-                                        $('#cData').trigger('click');
-                                        alert(response.responseText);
-                                    }
-                                    else {
-                                        $("#FormError").show();
-                                        $("#FormError>td").html(response.responseText);
-                                    }
-                                }
-                            }
-                        }
+                        editOptions: editOptions
                     }
                 },
             { key: false, name: 'UserName', index: 'UserName', editable: false, editoptions: { size: dbEditWidth } },
             { key: false, name: 'UserID', key: true, hidden: true, index: 'UserID', editable: true, editoptions: { size: dbEditWidth } },
             { key: false, name: 'FirstName', index: 'FirstName', editable: true, editoptions: { size: dbEditWidth } },
-            { key: false, name: 'LastName', index: 'LastName', editable: true, editoptions: { size: dbEditWidth }},
+            { key: false, name: 'LastName', index: 'LastName', editable: true, editoptions: { size: dbEditWidth } },
             { key: false, name: 'EmailAddress', index: 'EmailAddress', editable: true, editoptions: { size: dbEditWidth } },
             { key: false, name: 'Password', index: 'Password', editable: true, editoptions: { size: dbEditWidth } },
-            { key: false, name: 'Active', index: 'Active', editable: true
+            {
+                key: false, name: 'Active', index: 'Active', editable: true
                 , formatoptions: { disabled: true }, edittype: "checkbox"
-                , editoptions: { value: "Y:N"}
+                , editoptions: { value: "Y:N" }
             }],
         pager: jQuery('#pager'),
+        ondblClickRow: function (rowid) {
+            jQuery(this).jqGrid('editGridRow', rowid, editOptions);
+        },
         rowNum: 10,
         rowList: [2, 5, 10, 15],
         height: '100%',
