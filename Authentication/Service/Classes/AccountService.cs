5e3747189da95e39e1952b119b4f530a103f3cfc
vineelp <vineelp@gmail.com>
2015-12-12 09:59:48 -0500

    added where filters using expressions

--- Authentication/Service/Classes/AccountService.cs
+++ Authentication/Service/Classes/AccountService.cs
@@ -6,6 +6,7 @@ using Authentication.Models;
 using Authentication.DAL;
 using Authentication.Service.Interfaces;
 using AutoMapper;
+using System.Linq.Expressions;
 
 namespace Authentication.Service.Classes
 {
@@ -42,36 +43,40 @@ namespace Authentication.Service.Classes
             return true;
         }
 
-        public UserViewModel[] GetUsers(string managerName, string sidx, string sortOrder, int page, int pageSize, string userName, string firstName, string lastName, out int totalPages, out int totalRecords)
+        public UserViewModel[] GetUsers(string managerName, string sortIndex, string sortOrder, int page, int pageSize, string userName, string firstName, string lastName, out int totalPages, out int totalRecords)
         {
             List<int> managerLocations = null;
-            if (string.IsNullOrEmpty(managerName))
+            if (!string.IsNullOrEmpty(managerName))
             {
                 int managerID = unitOfWork.UserRepository.Get().FirstOrDefault(u => u.UserName.Equals(managerName)).UserID;
                 managerLocations = unitOfWork.MLocationRepository.Get().Where(u => u.ManagerID == managerID).Select(u => u.LocationID).ToList();
             }
+            Expression<Func<User, bool>> usersFilter = (u => u.UserName.Contains(userName) && u.FirstName.Contains(firstName) && u.LastName.Contains(lastName));
+            Expression<Func<User, bool>> managersFilter = null;
+            if(managerLocations != null)
+                managersFilter = (u => managerLocations.Contains(u.LocationID.Value));
+            userName = userName ?? string.Empty;
+            firstName = firstName ?? string.Empty;
+            lastName = lastName ?? string.Empty;
+
             IQueryable<User> userList = unitOfWork.UserRepository.Get();
             int pageIndex = Convert.ToInt32(page) - 1;
-            if (managerLocations != null)
-                userList = userList.Where(u => managerLocations.Contains(u.LocationID.Value));
-            if (!string.IsNullOrEmpty(userName))
-                userList = userList.Where(u => u.UserName.Contains(userName));
-            if (!string.IsNullOrEmpty(firstName))
-                userList = userList.Where(u => u.FirstName.Contains(firstName));
-            if (!string.IsNullOrEmpty(lastName))
-                userList = userList.Where(u => u.LastName.Contains(lastName));
+            userList = userList.Where(usersFilter);
+            if(managersFilter != null)
+                userList = userList.Where(managersFilter);
+
             totalRecords = userList.Count();
             totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
 
-            if (!string.IsNullOrEmpty(sidx))
+            if (!string.IsNullOrEmpty(sortIndex))
             {
                 if (sortOrder.ToUpper() == "DESC")
-                    userList = userList.OrderBy(sidx+ " descending");
+                    userList = userList.OrderBy(sortIndex+ " descending");
                 else
-                    userList = userList.OrderBy(sidx);
+                    userList = userList.OrderBy(sortIndex);
             }
             else
-                userList = userList.OrderBy("UserName");
+                userList = userList.OrderBy(u => u.UserName);
             userList = userList.Skip(pageIndex * pageSize).Take(pageSize);
 
             List<User> filteredUsers = userList.ToList();
